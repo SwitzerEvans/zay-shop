ba10aa34a556105d1c00cf9a9536150b383841d4
SwitzerEvans <chiemekaevans928@gmail.com>
2025-12-04 05:36:20 +0100

    Update hamburger.js

--- js/hamburger.js
+++ js/hamburger.js
@@ -25,32 +25,3 @@ serviceLists.forEach(serviceList => {
         span.style.color = 'black';
     });
 });
-
-
-
-
-// slider for home page
-
-    let slides = document.querySelectorAll(".heroslides");
-    let index = 0;
-
-    // Next button
-    document.getElementById("next").addEventListener("click", function () {
-        slides[index].classList.remove("active");
-        index = (index + 1) % slides.length;
-        slides[index].classList.add("active");
-    });
-
-    // Prev button
-    document.getElementById("prev").addEventListener("click", function () {
-        slides[index].classList.remove("active");
-        index = (index - 1 + slides.length) % slides.length;
-        slides[index].classList.add("active");
-    });
-
-    // Auto slide every 3 seconds
-    setInterval(() => {
-        slides[index].classList.remove("active");
-        index = (index + 1) % slides.length;
-        slides[index].classList.add("active");
-    }, 3000);
